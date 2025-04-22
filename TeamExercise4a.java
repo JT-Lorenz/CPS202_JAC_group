edd6cbd095e24705dff83ac03185fa5cc448482f
JT-Lorenz <jakinlorenz@gmail.com>
2025-04-22 19:23:06 -0400

    Initial work on the GUI of the game

---
+++ TeamExercise4a.java
@@ -0,0 +1,11 @@
+package teamExercises;
+import javax.swing.*;
+
+public class TeamExercise4a {
+
+	public static void main(String [] args)
+	{
+		JFrame win = new TeamExercise4b();
+		win.setVisible(true);
+	}
+}
